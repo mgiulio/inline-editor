887544105debe1d0c7730eb27dc68bcadb8e2b34
mgiulio <giulio.mainardi@gmail.com>
2015-06-16 11:38:32 +0200

    Define a constructor for the editors

--- inline-editors.js
+++ inline-editors.js
@@ -1,113 +1,89 @@
 (function() {
 	
-	var 
-		filter = Array.prototype.filter
-	;
+	installCSS();
 	
-	function install(cfg) {
-		installCSS();
+	function C(cfg) {
+		this.el = cfg.el;
+		this.processNewText = cfg.processNewText;
 		
-		var 
-			elOriginalDisplayMode,
-			editables = document.querySelectorAll(cfg.selector) 
+		var editor = this.editor = document.createElement('div');
+		editor.className = 'editor';
+		editor.innerHTML = `
+			<textarea></textarea>
+			<div class="toolbar">
+				<button class="button submit">Save</button>
+				<button class="button cancel">Cancel</button>
+			</div>
+			<div class="overlay">
+				<div class="msg">${cfg.activityIndicator.message}</div>
+			</div>
+		`;
+			
+		var textArea = this.textArea = editor.querySelector('textArea');
+		var submit = this.submit = editor.querySelector('.button.submit');
+		var cancel = this.cancel = editor.querySelector('.button.cancel');
+			
+		this.el.addEventListener('click', this.enterEditMode.bind(this), false);
+		submit.addEventListener('click', this.submitListener.bind(this), false);
+		cancel.addEventListener('click', this.cancelListener.bind(this), false);
+			
+		this.el.parentNode.insertBefore(editor, this.el.nextSibling);
+	}
+	
+	C.prototype.enterEditMode = function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+	
+		var text = this.el.innerHTML;
+		this.textArea.value = text;
+			
+		this.show();
+	};
+	
+	C.prototype.submitListener = function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+			
+		this.editor.classList.add('blocked');
+			
+		var newText = this.textArea.value;
+			
+		this.processNewText(newText, this.el)
+			.then(
+				function(processedText) {
+					this.el.innerHTML = processedText;
+					
+				}.bind(this),
+				function() {
+				}.bind(this)
+			)
+			.then(function() {
+				this.editor.classList.remove('blocked');
+				this.hide();
+			}.bind(this))
 		;
+	};
 		
-		filter.call(editables, function(el) { return !isInstalled(el); }).forEach(installEditor);
+	C.prototype.cancelListener = function(e) {	
+		e.preventDefault();
+		e.stopPropagation();
+			
+		this.hide();
+	};
 		
-		function installEditor(editableEl) {
-			var el = {
-				editable: editableEl
-			};
+	C.prototype.show = function() {
+		this.elOriginalDisplayMode = window.getComputedStyle(this.el, null).display;
+		this.el.style.display = 'none';
 			
-			el.editor = document.createElement('div');
-			el.editor.className = 'editor';
-			el.editor.innerHTML = `
-				<textarea></textarea>
-				<div class="toolbar">
-					<button class="button submit">Save</button>
-					<button class="button cancel">Cancel</button>
-				</div>
-				<div class="overlay">
-					<div class="msg">${cfg.activityIndicator.message}</div>
-				</div>
-			`;
-			
-			el.textArea = el.editor.querySelector('textArea');
-			el.submit = el.editor.querySelector('.button.submit');
-			el.cancel = el.editor.querySelector('.button.cancel');
-			
-			el.editable.addEventListener('click', enterEditMode, false);
-			el.submit.addEventListener('click', submit, false);
-			el.cancel.addEventListener('click', cancel, false);
-			
-			el.editable.parentNode.insertBefore(el.editor, el.editable.nextSibling);
-			
-			function enterEditMode(e) {
-				e.preventDefault();
-				e.stopPropagation();
+		this.editor.classList.add('visible');
 		
-				var text = el.editable.innerHTML;
-				el.textArea.value = text;
-				
-				show();
-			}
-			
-			function submit(e) {
-				e.preventDefault();
-				e.stopPropagation();
-				
-				el.editor.classList.add('blocked');
-				
-				var newText = el.textArea.value;
-				
-				cfg.processNewText(el.editable, newText)
-					.then(
-						function(processedText) {
-							el.editable.innerHTML = processedText;
-							
-						},
-						function() {
-						}
-					)
-					.then(function() {
-						el.editor.classList.remove('blocked');
-						hide();
-					})
-				;
-			}
-			
-			function cancel(e) {
-				e.preventDefault();
-				e.stopPropagation();
-				
-				hide();
-			}
-			
-			function show() {
-				elOriginalDisplayMode = window.getComputedStyle(el.editable, null).display;
-				el.editable.style.display = 'none';
-				
-				el.editor.classList.add('visible');
-				
-				el.textArea.select();
-			}
-			
-			function hide() {
-				el.editor.classList.remove('visible');
-				
-				el.editable.style.display = elOriginalDisplayMode;
-			}
-		}
-	}
-	
-	function isInstalled(editableEl) {
-		var nextEl = editableEl.nextElementSibling;
+		this.textArea.select();
+	};
 		
-		if (!nextEl)
-			return false;
-		
-		return nextEl.classList.contains('editor');
-	}
+	C.prototype.hide = function() {
+		this.editor.classList.remove('visible');
+		this.el.style.display = this.elOriginalDisplayMode;
+	};
 	
 	function installCSS() {
 		var styleEl = document.createElement('style');
@@ -159,15 +135,8 @@
 		styleEl.textContent = css;
 		
 		document.head.appendChild(styleEl);
-		
-		installCSS = function() {};
 	}
 		
-	var api = {
-		install: install,
-		//uninstall
-	};
-	
-	window.inlineEditor = api;
+	window.InlineEditor = C;
 
 })();
