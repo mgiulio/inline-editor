5551c26e93b9fede0b6d8776fac7bd2f2b0cbde8
mgiulio <giulio.mainardi@gmail.com>
2015-06-16 19:08:47 +0200

    Demo: profile page

---
+++ demo/profile-page/script.js
@@ -0,0 +1,57 @@
+(function() {
+	
+	init();
+	
+	function init() {
+		var mastheadTitleEditor = new InlineEditor({
+			el: document.querySelector('.masthead__title'),
+			processNewText: sendToServer,
+			activityIndicator: {
+				message: 'Saving header title...'
+			}
+		});
+
+		var mastheadSubTitleEditor = new InlineEditor({
+			el: document.querySelector('.masthead__subtitle'),
+			processNewText: sendToServer,
+			activityIndicator: {
+				message: 'Saving header title...'
+			}
+		});
+
+		var aboutDescEditor = new InlineEditor({
+			el: document.querySelector('.aux .about .desc'),
+			processNewText: sendToServer,
+			activityIndicator: {
+				message: 'Saving about desc...'
+			}
+		});
+	}
+	
+	function sendToServer(text, el) {
+		var
+			rnd = Math.random
+			minTime = 1,
+			maxTime = 5,
+			successProb = 0.9
+		;
+		return new Promise(function(resolve, reject) {
+			var json = {
+				el: el,
+				text: text
+			};
+			
+			console.log(json);
+			
+			setTimeout(function() {
+				if (rnd() <= successProb)
+					resolve(text);
+				else {
+					console.log('failure');
+					reject('Cannot save text');
+				}
+			}, minTime + rnd()*maxTime * 1000);
+		})
+	}
+	
+}());
